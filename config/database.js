a02e6ae9b483208d1c6f38857c842efafa3bccc4
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2023-03-30 17:41:27 +0100

    # initial commit (YEEEEY)

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// Replace `blog_app` with your own database name
+mongoose.connect('mongodb://localhost/blog_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Check if the connection is successful
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB database!');
+});
+
+module.exports = db;
